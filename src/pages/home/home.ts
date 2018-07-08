a72b95f84f511a1029f0c8d95a68f6b7651ba728
Stevie <webebrickit@gmail.com>
2018-07-07 22:30:36 -0700

    this is working but i want to try something else

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  video_id: any;
 
+  constructor(public navCtrl: NavController,
+    public sanitizer: DomSanitizer) {
+
+      this.video_id = 'tkzY_VwNIek';
+      // let tag = document.createElement('script');
+
+      // tag.src = "https://www.youtube.com/iframe_api";
+      // let firstScriptTag = document.getElementsByTagName('script')[0];
+      // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   }
+
+
+  ionViewDidLoad() {
+      console.log('ionViewDidLoad VideoSinglePage');
+      this.datBoi();
+  }
+
+  updateVideoUrl(id: string) {
+      // Appending an ID to a YouTube URL is safe.
+      // Always make sure to construct SafeValue objects as
+      // close as possible to the input data, so
+      // that it's easier to check if the value is safe.
+      let dangerousVideoUrl = 'https://www.youtube.com/embed/' + id + '?rel=0&showinfo=0&controls=0';
+      return this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
+      
+  }
+
+  datBoi() {
+      console.log(document.getElementById('mainBoi'));
+  }
+
+  // onYouTubeIframeAPIReady() {
+  //   this.player = new YT.Player('player', {
+  //     height: '390',
+  //     width: '640',
+  //     videoId: 'M7lc1UVf-VE',
+  //     events: {
+  //       'onReady': onPlayerReady,
+  //       'onStateChange': onPlayerStateChange
+  //     }
+  //   });
+  // }
+  
 
 }
